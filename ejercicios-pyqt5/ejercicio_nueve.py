7489f698e74ceb0b8e164682a8eded85bbc4cf3d
Miguel Alejandro Lescano <alescano.test@gmail.com>
2023-02-21 13:46:48 -0300

    pidiendo todos los datos

--- ejercicios-pyqt5/ejercicio_nueve.py
+++ ejercicios-pyqt5/ejercicio_nueve.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidgetItem, QMessageBox, QInputDialog
 from PyQt5 import uic
 
 '''
@@ -14,22 +14,50 @@ class MiVentana(QMainWindow):
         self.btnEliminar.clicked.connect(self.eliminar_persona)
 
     def agregar_persona(self):
-        self.tablaDatos.insertRow(0)
-        self.tablaDatos.setItem(0, 0, QTableWidgetItem(self.nombre.text()))
-        self.tablaDatos.setItem(0, 1, QTableWidgetItem(self.apellido.text()))
-        self.tablaDatos.setItem(0, 2, QTableWidgetItem(self.email.text()))
-        self.borrar_entradas() 
+        if self.nombre.text() and self.apellido.text() and self.email.text() != '':
+            self.tablaDatos.insertRow(0)
+            self.tablaDatos.setItem(0, 0, QTableWidgetItem(self.nombre.text()))
+            self.tablaDatos.setItem(0, 1, QTableWidgetItem(self.apellido.text()))
+            self.tablaDatos.setItem(0, 2, QTableWidgetItem(self.email.text()))
+            self.borrar_entradas() 
+        else:
+            msg = QMessageBox(QMessageBox.Warning, "Error", "Todos los campos están vacíos.")
+            msg.exec()
 
     def editar_persona(self):
         fila = self.tablaDatos.currentRow()
-        self.nombre.setText(self.tablaDatos.item(fila ,0).text())
-        self.apellido.setText(self.tablaDatos.item(fila ,1).text())
-        self.email.setText(self.tablaDatos.item(fila ,2).text())
-        #self.tablaDatos.item()
+        #self.nombre.setText(self.tablaDatos.item(fila ,0).text())
+        #self.apellido.setText(self.tablaDatos.item(fila ,1).text())
+        #self.email.setText(self.tablaDatos.item(fila ,2).text())
+        for i in range(2):
+            if i == 0:
+                texto, ok = QInputDialog.getText(self, "Nombre", "Ingresar el nuevo nombre:")
+                if ok and texto != '':
+                    self.tablaDatos.setItem(fila, i, QTableWidgetItem(texto))
+                else:
+                    msg = QMessageBox(QMessageBox.Warning, "Error", "Por favor, ingrese el nombre.")
+                    msg.exec()
+            if i == 1:
+                texto, ok = QInputDialog.getText(self, "Nombre", "Ingresar el nuevo nombre:")
+                if ok and texto != '':
+                    self.tablaDatos.setItem(fila, i, QTableWidgetItem(texto))
+                    msg = QMessageBox(QMessageBox.Warning, "Error", "Por favor, ingrese el apellido.")
+                    msg.exec()
+            if i == 2:
+                texto, ok = QInputDialog.getText(self, "Nombre", "Ingresar el nuevo email:")
+                if ok and texto != '':
+                    self.tablaDatos.setItem(fila, i, QTableWidgetItem(texto))
+                    msg = QMessageBox(QMessageBox.Warning, "Error", "Por favor, ingrese el email.")
+                    msg.exec()
+            
+
+
+
 
     def eliminar_persona(self):
         fila = self.tablaDatos.currentRow()
         self.tablaDatos.removeRow(fila)
+        self.borrar_entradas()
 
     def borrar_entradas(self):
         self.nombre.clear()
